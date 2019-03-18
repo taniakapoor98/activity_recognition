88f6a14965241d462afddb98c0b3cd0bbdd7d00e
taniakapoor98 <42332049+taniakapoor98@users.noreply.github.com>
2019-03-18 16:16:30 +0530

    Add files via upload

---
+++ fus.py
@@ -0,0 +1,338 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import tensorflow as tf  # Version 1.0.0 (some previous versions are used in past commits)
+from sklearn import metrics
+from pathlib import Path
+import os
+
+
+# Output classes to learn how to classify
+LABELS_mapping = {
+    "WALKING": 1, 
+    "SITTING": 2, 
+   
+}
+
+DATA_PATH = Path("data/")
+
+DATASET_PATH = DATA_PATH + "fused_Dataset/"
+print("\n" + "Dataset is now located at: " + DATASET_PATH)
+
+TRAIN = "train/"
+TEST = "test/"
+
+
+def load_X(X_signals_paths):
+    X_signals = []
+    
+    for signal_type_path in X_signals_paths:
+        file = open(signal_type_path, 'r')
+        # Read dataset from disk, dealing with text files' syntax
+        X_signals.append(
+            [np.array(serie, dtype=np.float32) for serie in [
+                row.replace('  ', ' ').strip().split(' ') for row in file
+            ]]
+        )
+        file.close()
+    
+    return np.transpose(np.array(X_signals), (1, 2, 0))
+
+X_train_signals_paths = [
+    DATASET_PATH + TRAIN + "Inertial Signals/" + signal + "train.txt" for signal in INPUT_SIGNAL_TYPES
+]
+X_test_signals_paths = [
+    DATASET_PATH + TEST + "Inertial Signals/" + signal + "test.txt" for signal in INPUT_SIGNAL_TYPES
+]
+
+X_train = load_X(X_train_signals_paths)
+X_test = load_X(X_test_signals_paths)
+
+
+# Load "y" (the neural network's training and testing outputs)
+
+def load_y(y_path):
+    file = open(y_path, 'r')
+    # Read dataset from disk, dealing with text file's syntax
+    y_ = np.array(
+        [elem for elem in [
+            row.replace('  ', ' ').strip().split(' ') for row in file
+        ]], 
+        dtype=np.int32
+    )
+    file.close()
+    
+    # Substract 1 to each output class for friendly 0-based indexing 
+    return y_ - 1
+
+y_train_path = DATASET_PATH + TRAIN + "y_train.txt"
+y_test_path = DATASET_PATH + TEST + "y_test.txt"
+
+y_train = load_y(y_train_path)
+y_test = load_y(y_test_path)
+
+
+def tokenize(sent):
+    words = sent.split(' ')
+    tokenized = []
+    for word in words:
+        if LABELS_mapping.get(word) != None:
+            tokenized.append(LABELS_mapping[word])
+        else:
+            tokenized.append(0)
+    return tokenized
+
+def extract_batch_size(_train, step, batch_size):
+    # Function to fetch a "batch_size" amount of data from "(X|y)_train" data. 
+    
+    shape = list(_train.shape)
+    shape[0] = batch_size
+    batch_s = np.empty(shape)
+
+    for i in range(batch_size):
+        # Loop index
+        index = ((step-1)*batch_size + i) % len(_train)
+        batch_s[i] = _train[index] 
+
+    return batch_s
+
+learning_rate = 0.0025
+lambda_loss_amount = 0.0015
+training_iters = training_data_count * 300  # Loop 300 times on the dataset
+batch_size = 1500
+display_iter = 300 # To show test set accuracy during training
+
+def LSTM_RNN(_X, _weights, _biases):
+   
+    _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
+    # Reshape to prepare input to hidden activation
+    _X = tf.reshape(_X, [-1, n_input]) 
+    # new shape: (n_steps*batch_size, n_input)
+    
+    # ReLU activation, thanks to Yu Zhao for adding this improvement here:
+    _X = tf.nn.relu(tf.matmul(_X, _weights['hidden']) + _biases['hidden'])
+    # Split data because rnn cell needs a list of inputs for the RNN inner loop
+    _X = tf.split(_X, n_steps, 0) 
+
+    lstm_cell_1 = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cell_2 = tf.contrib.rnn.BasicLSTMCell(n_hidden, forget_bias=1.0, state_is_tuple=True)
+    lstm_cells = tf.contrib.rnn.MultiRNNCell([lstm_cell_1, lstm_cell_2], state_is_tuple=True)
+   
+    outputs, states = tf.contrib.rnn.static_rnn(lstm_cells, _X, dtype=tf.float32)
+
+
+    lstm_last_output = outputs[-1]
+    
+    # Linear activation
+    return tf.matmul(lstm_last_output, _weights['out']) + _biases['out']
+
+
+
+def one_hot(y_, n_classes=n_classes):
+    # Function to encode neural one-hot output labels from number indexes 
+    # one_hot(y_=[[5], [0], [3]], n_classes=6):
+    #     return [[0, 0, 0, 0, 0, 1], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0]]
+    
+    y_ = y_.reshape(len(y_))
+    return np.eye(n_classes)[np.array(y_, dtype=np.int32)]  # Returns FLOATS
+
+x = tf.placeholder(tf.float32, [None, n_steps, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+
+    print('Creating data...')
+tokenizedTrain = []
+for sent in TRAIN:
+    tokenizedTrain.append(tokenize(sent))
+
+
+X_train = np.array(tokenizedTrain)
+y_train = np.array(train_arr)
+
+tokenizedTest = []
+for sent in testSentences:
+    tokenizedTest.append(tokenize(sent))
+
+X_test = np.array(tokenizedTest)
+y_test = np.array(test_arr)
+#build moel,,,,,.......................................
+n_hidden = 32 # Hidden layer num of features
+n_classes = 2
+
+x = tf.placeholder(tf.float32, [None, n_steps, n_input])
+y = tf.placeholder(tf.float32, [None, n_classes])
+# Graph weights
+weights = {
+    'hidden': tf.Variable(tf.random_normal([n_input, n_hidden])), # Hidden layer weights
+    'out': tf.Variable(tf.random_normal([n_hidden, n_classes], mean=1.0))
+}
+biases = {
+    'hidden': tf.Variable(tf.random_normal([n_hidden])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+pred = LSTM_RNN(x, weights, biases)
+
+# Loss, optimizer and evaluation
+l2 = lambda_loss_amount * sum(
+    tf.nn.l2_loss(tf_var) for tf_var in tf.trainable_variables()
+) # L2 loss prevents this overkill neural network to overfit the data
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=pred)) + l2 # Softmax loss
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Adam Optimizer
+
+correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+
+
+
+
+# To keep track of training's performance
+test_losses = []
+test_accuracies = []
+train_losses = []
+train_accuracies = []
+
+# Launch the graph
+sess = tf.InteractiveSession(config=tf.ConfigProto(log_device_placement=True))
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# Perform Training steps with "batch_size" amount of example data at each loop
+step = 1
+while step * batch_size <= training_iters:
+    batch_xs =         extract_batch_size(X_train, step, batch_size)
+    batch_ys = one_hot(extract_batch_size(y_train, step, batch_size))
+
+    # Fit training using batch data
+    _, loss, acc = sess.run(
+        [optimizer, cost, accuracy],
+        feed_dict={
+            x: batch_xs, 
+            y: batch_ys
+        }
+    )
+    train_losses.append(loss)
+    train_accuracies.append(acc)
+    
+    # Evaluate network only at some steps for faster training: 
+    if (step*batch_size % display_iter == 0) or (step == 1) or (step * batch_size > training_iters):
+        
+        # To not spam console, show training accuracy/loss in this "if"
+        print("Training iter #" + str(step*batch_size) +               ":   Batch Loss = " + "{:.6f}".format(loss) +               ", Accuracy = {}".format(acc))
+        
+        # Evaluation on the test set (no learning made here - just evaluation for diagnosis)
+        loss, acc = sess.run(
+            [cost, accuracy], 
+            feed_dict={
+                x: X_test,
+                y: one_hot(y_test)
+            }
+        )
+        test_losses.append(loss)
+        test_accuracies.append(acc)
+        print("PERFORMANCE ON TEST SET: " +  "Batch Loss = {}".format(loss) + ", Accuracy = {}".format(acc))
+
+    step += 1
+
+print("Optimization Finished!")
+
+
+
+# Accuracy for test data
+
+one_hot_predictions, accuracy, final_loss = sess.run(
+    [pred, accuracy, cost],
+    feed_dict={
+        x: X_test,
+        y: one_hot(y_test)
+    }
+)
+
+test_losses.append(final_loss)
+test_accuracies.append(accuracy)
+
+print("FINAL RESULT: " +       "Batch Loss = {}".format(final_loss) +       ", Accuracy = {}".format(accuracy))
+
+
+# (Inline plots: )
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+font = {
+    'family' : 'Bitstream Vera Sans',
+    'weight' : 'bold',
+    'size'   : 18
+}
+matplotlib.rc('font', **font)
+
+width = 12
+height = 12
+plt.figure(figsize=(width, height))
+
+indep_train_axis = np.array(range(batch_size, (len(train_losses)+1)*batch_size, batch_size))
+plt.plot(indep_train_axis, np.array(train_losses),     "b--", label="Train losses")
+plt.plot(indep_train_axis, np.array(train_accuracies), "g--", label="Train accuracies")
+
+indep_test_axis = np.append(
+    np.array(range(batch_size, len(test_losses)*display_iter, display_iter)[:-1]),
+    [training_iters]
+)
+plt.plot(indep_test_axis, np.array(test_losses),     "b-", label="Test losses")
+plt.plot(indep_test_axis, np.array(test_accuracies), "g-", label="Test accuracies")
+
+plt.title("Training session's progress over iterations")
+plt.legend(loc='upper right', shadow=True)
+plt.ylabel('Training Progress (Loss or Accuracy values)')
+plt.xlabel('Training iteration')
+
+plt.show()
+
+    
+# ## And finally, the multi-class confusion matrix and metrics!
+
+ 
+# Results
+
+predictions = one_hot_predictions.argmax(1)
+
+print("Testing Accuracy: {}%".format(100*accuracy))
+
+print("")
+print("Precision: {}%".format(100*metrics.precision_score(y_test, predictions, average="weighted")))
+print("Recall: {}%".format(100*metrics.recall_score(y_test, predictions, average="weighted")))
+print("f1_score: {}%".format(100*metrics.f1_score(y_test, predictions, average="weighted")))
+
+print("")
+print("Confusion Matrix:")
+confusion_matrix = metrics.confusion_matrix(y_test, predictions)
+print(confusion_matrix)
+normalised_confusion_matrix = np.array(confusion_matrix, dtype=np.float32)/np.sum(confusion_matrix)*100
+
+print("")
+print("Confusion matrix (normalised to % of total test data):")
+print(normalised_confusion_matrix)
+print("Note: training and testing data is not equally distributed amongst classes, ")
+print("so it is normal that more than a 6th of the data is correctly classifier in the last category.")
+
+# Plot Results: 
+width = 12
+height = 12
+plt.figure(figsize=(width, height))
+plt.imshow(
+    normalised_confusion_matrix, 
+    interpolation='nearest', 
+    cmap=plt.cm.rainbow
+)
+plt.title("Confusion matrix \n(normalised to % of total test data)")
+plt.colorbar()
+tick_marks = np.arange(n_classes)
+plt.xticks(tick_marks, LABELS_mapping, rotation=90)
+plt.yticks(tick_marks, LABELS_mapping)
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+
+# Training 
+
+
